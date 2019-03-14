85a7dbe9fd071cd3fb1362274c13b0942893e7fa
dannibla <dannibla@outlook.com>
2019-03-14 10:28:24 +0530

    updated latest version

--- app.js
+++ app.js
@@ -1,47 +1,26 @@
 const express = require('express');
-const pg = require("pg");
-const bodyParser = require("body-parser");
-const path = require('path');
+const { Client } = require('pg');
+const connectionString = 'postgres://postgres:root@localhost:5432/database';
 
-var customers = require('./routes/customers'); 
-var routes = require('./routes');
-var app = express();
- 
-var connectionString = "postgres://postgres:root@localhost:5432/database";
-
-app.set('port', process.env.PORT || 4000);
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'ejs');
-
-app.use(bodyParser.json());
-app.use(bodyParser.urlencoded({ extended: true }));
-
-app.use(express.static(path.join(__dirname, 'public')));
-
-app.get('/json', function (req, res, next) {
-    pg.connect(connectionString,function(err,client,done) {
-       if(err){
-           console.log("not able to get connection "+ err);
-           res.status(400).send(err);
-       } 
-       client.query('SELECT * FROM student where id = $1', [1],function(err,result) {
-           done(); // closing the connection;
-           if(err){
-               console.log(err);
-               res.status(400).send(err);
-           }
-           res.status(200).send(result.rows);
-       });
-    });
+const client = new Client({
+    connectionString: connectionString
 });
 
-app.get('/', routes.index);
-app.get('/customers', customers.list);
-app.get('/customers/add', customers.add);
-app.post('/customers/add', customers.save);
-app.get('/customers/delete/:id', customers.delete);
-app.get('/customers/edit/:id', customers.edit);
-app.post('/customers/edit/:id', customers.update);
+client.connect();
+
+var app = express();
+
+app.set('port', process.env.PORT || 4000);
+
+app.get('/', function (req, res, next) {
+    client.query('SELECT * FROM student where id = $1', [1], function (err, result) {      
+        if (err) {
+            console.log(err);
+            res.status(400).send(err);
+        }
+        res.status(200).send(result.rows); 
+    });
+});
 
 app.listen(4000, function () {
     console.log('Server is running.. on Port 4000');
